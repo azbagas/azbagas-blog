c0425bd536b4bb83cd761d0e61d2f2b929215c84
azbagas <azharbagaskara1@gmail.com>
2026-02-18 19:49:27 +0700

    fix: update Disqus component to use canonical URL for page identification

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,12 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), icon()]
+  site: 'https://azbagas.com',
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
 });
